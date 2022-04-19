521ab85e14cf4bd060d0f1e93aa6a135bf23b4f9
moneshadhali <99983599+moneshadhali@users.noreply.github.com>
2022-04-19 12:59:35 +0100

    Created functions for Password
    Created functions for Password inside the LoginRegisterCont file

--- PolyGlot/src/main/java/controllers/LoginRegisterCont.java
+++ PolyGlot/src/main/java/controllers/LoginRegisterCont.java
@@ -1,11 +1,16 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
+
 package controllers;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.nio.charset.StandardCharsets;
+import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
+import java.security.spec.InvalidKeySpecException;
+import java.util.Base64;
+import java.util.Random;
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
 import javax.swing.JOptionPane;
 import models.UserMDL;
 import views.MenuBarV;
@@ -15,10 +20,7 @@ import views.LoginV;
 import views.ProgV;
 import views.SupportV2;
 
-/**
- *
- * @author Amit
- */
+
 public class LoginRegisterCont implements ActionListener
 {
     private LoginV loginPage;
@@ -88,5 +90,63 @@ public class LoginRegisterCont implements ActionListener
         return true;
     }
     
+    
+    
+    
+    //Methods by Monesha
+    //GENERATING SECURE PASSWORDS 
+    private final int ITERATIONS = 100;
+    private final int KEY_LENGHT =256;
+    
+    public static String getSalt(int lenght) throws NoSuchAlgorithmException{
+        //Secure Random generator
+        SecureRandom secureRandom = SecureRandom.getInstance("SHA1PRNG");
+        byte[] salt = new byte[16];
+        secureRandom.nextBytes(salt); //Generates salt
+        String finalSalt = new String(salt,StandardCharsets.UTF_8);
+        return finalSalt;
+    }
+    
+    //Generate Hash of the password
+    public byte[] hash(char[] password, byte[] salt){
+        PBEKeySpec spec = new PBEKeySpec(password,salt,ITERATIONS,KEY_LENGHT);
+        try{
+            SecretKeyFactory skf = SecretKeyFactory.getInstance("PBKDF2WithHmacSHA512");
+            byte[] hash=skf.generateSecret(spec).getEncoded();
+            return hash;
+        }catch(NoSuchAlgorithmException|InvalidKeySpecException e){
+            throw new AssertionError("Error while hashing a password"+e.getMessage(),e);
+        }finally{
+            spec.clearPassword();
+            System.gc();
+        }
+    }
+    
+    public String generateSecurePwd(String password, String salt){
+        //System.out.println("Password supplied: "+password);
+        byte[] securePassword = hash(password.toCharArray(),salt.getBytes());
+        String returnValue = Base64.getEncoder().encodeToString(securePassword);
+        return salt + ":" + returnValue;
+    }
+    
+    public boolean isValidPwd(String providedPwd, String storedPWD){
+        boolean returnValue= false;
+        String[] params = storedPWD.split(":");
+        String sameSalt = params[0];
+        
+        //Generating new secure password with the same salt
+        String newSecurePWD = generateSecurePwd(providedPwd,sameSalt);
+        returnValue = newSecurePWD.equalsIgnoreCase(storedPWD);
+        return returnValue; //IF TRUE password matches
+    }
+    
+    //NEXT STEPS (for Monesha and Amit)
+    //GET User password from the database
+    //Input it into the function isValidPwd as a STRING
+    //If the resutl of function isValidPwd is true THEN LOGIN
+    //else Password incorrect
+    
+    //GENERATE password using generateSecurePwd
+    //INPUT it into Database
     
 }
