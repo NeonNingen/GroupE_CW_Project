dafba6f5a4133a71b3f677903af8070722f33279
moneshadhali <99983599+moneshadhali@users.noreply.github.com>
2022-05-03 16:24:50 +0100

    main page/class
    comments deleted and formatted

--- PolyGlot/src/main/java/models/Main.java
+++ PolyGlot/src/main/java/models/Main.java
@@ -1,40 +1,21 @@
-
 package models;
 
 import controllers.LoginRegisterCont;
 
-public class Main 
-{
-    public static void main(String[] args) 
-    {
-        //currentUser= new UserMDL();
-        //to test that everything works, UserModel was used to test the methods
+public class Main {
+
+    public static void main(String[] args) {
         UserMDL user = new UserMDL();
-        
-        LoginRegisterCont logRegCont= new LoginRegisterCont(user);
-        
+        LoginRegisterCont logRegCont = new LoginRegisterCont(user);
         logRegCont.startApp();
         //currentUser= logRegCont.getUserInfo();
-        
-        
+
         //LoginV loginPage = new LoginV();
         //loginPage.show();
-
-       //calling the methodss
-       //user.insert();
-       //user.retrieveUserList();
-       //user.delete();
-       //user.update();
-        
-    }  
-//    
-//    
-//    /**
-//     * Suggested by Aisana
-//     * Used to set up current user info
-//     * @param user 
-//     */
-//    public void recordUser(UserMDL user){
-//        this.currentUser= user;
-//    }
+        //calling the methodss
+        //user.insert();
+        //user.retrieveUserList();
+        //user.delete();
+        //user.update();
+    }
 }
